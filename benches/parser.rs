5734033b49e5f9c23cc38b8366b94a248e9e21b2
cijiugechu <cijiugechu@gmail.com>
2023-09-06 14:21:51 +0800

    chore: add new metrics

--- benches/parser.rs
+++ benches/parser.rs
@@ -2,6 +2,12 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use nodejs_semver::{Range, Version};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("version parser", |b| {
+        b.iter(|| {
+            let _v = Version::parse(black_box("1.2.3-rc.4")).unwrap();
+        })
+    });
+
     c.bench_function("range", |b| {
         b.iter(|| {
             let range = Range::parse(black_box(">=1.2.3-rc.4")).unwrap();
